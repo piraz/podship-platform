6df43a116ea39d47da48ddc3ae9b8974f12aada7
Flavio Garcia <piraz@candango.org>
2017-04-14 00:08:08 -0400

    Moved and mapped dependencies from bower to yarn.

--- podship-platform/assets/stealconfig.js
+++ podship-platform/assets/stealconfig.js
@@ -6,12 +6,25 @@ System.config({
         bootstrap: "/vendor/bootstrap/js/bootstrap.js",
         i18next: "/vendor/i18next/i18next.js",
         "i18next/*": "/vendor/i18next/*",
+        "i18next-xhr-backend": "/vendor/i18next-xhr-backend/i18nextXHRBackend.js",
+        "i18next-xhr-backend/*": "/vendor/i18next-xhr-backend/*",
         jquery: "/vendor/jquery/jquery.js",
         "jquery/*": "/vendor/jquery/*.js",
+        "jquery-i18next": "/vendor/jquery-i18next/jquery-i18next.js",
+        "jquery-i18next/*": "/vendor/jquery-i18next/*.js",
+        "less/dist/less": "/vendor/less/less.js",
+        "$css": "/vendor/steal-css/css.js",
+        "steal-css": "/vendor/steal-css/css.js",
+        "steal-less": "/vendor/steal-less/less.js",
+        "steal-less": "/vendor/steal-less/less.js",
+        "less-engine": "/vendor/steal-less/less-engine.js",
         "login": "/assets/components/login/login.js",
         "stylesheets/*.less": "/assets/stylesheets/*.less"
     },
     ext: {
-        stache: 'can/view/stache/system'
+        stache: 'can/view/stache/system',
+        less: "steal-less",
+        $css: "steal-css",
+        css: "steal-css"
     }
 });
