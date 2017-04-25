6bb45197a44c52d67e892a0f3448549216679abc
Flavio Garcia <piraz@candango.org>
2017-04-25 19:43:54 -0400

    Converted the searchbox to can 3.
    Also fixed the dropdown click behaviour.

--- podship-platform/static/components/navbar/searchbox/searchbox.js
+++ podship-platform/static/components/navbar/searchbox/searchbox.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015-2016 Flavio Garcia
+ * Copyright 2015-2017 Flavio Garcia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,78 +14,106 @@
  * limitations under the License.
  */
 
-steal("jquery","can-component", "can-fixture/fixture.js", "can-event",
+steal("can-jquery","can-component", "can-define/map", "can-define/list",
+    "can-event", "can-fixture/fixture.js", "can-set/src/set.js",
     "./searchbox.stache",
-    function($, Component, fixture, can_event, template) {
+    function($, Component, DefineMap, DefineList, can_event, fixture, set,
+             template) {
     fixture({
         "POST /contact/search": function() {
             return "/assets/fixtures/search/results.json";
         }
     });
+
+    var SearchResult = DefineMap.extend({
+        id: "number",
+        img: "string",
+        label: "string"
+    });
+
+    var SearchResultList = DefineList.extend({
+        "#": SearchResult
+    });
     /*var SearchboxModel = can.Model.extend({
         findAll : "POST /contact/search"
     },{});*/
 
-    Component.extend({
-        tag: "nav-search-box",
-        template: template,
-        viewModel:{
-            error: false,
-            retults: [],
-            noContacts: false,
-            hasResults: false,
-            isLoading: true,
-            search: function() {
-                /*SearchboxModel.findAll(
-                    {}, function( response ){
-                        this.attr('hasResults', true);
-                        this.attr('results', response[0].results);
-                        this.attr('isLoading', false);
-                    }.bind(this)
-                );*/
+    var NavSearchViewModel = DefineMap.extend("NavSearchViewModel", {
+        error: 'boolean',
+        retults: 'observable',
+        hasResults: 'boolean',
+        isLoading: 'boolean',
+        previousValue: 'string',
+        searched: 'boolean',
+        showSearchDropdown: function (searchQuery, event) {
+            var previousValue = this.previousValue;
+            this.previousValue = searchQuery.val();
+            if(searchQuery.val().length > 0) {
+                searchQuery.parent().addClass("open");
+                if(previousValue != searchQuery.val()){
+                    this.isLoading = true;
+                    this.search();
+                    setTimeout(function() {
+                        this.searched = true;
+                        this.isLoading = false;
+                    }.bind(this), 500);
+                }
+                steal.dev.log("The search query size is bigger than 0." +
+                    "Showing the dropdwon.");
+            }
+            else{
+                this.searched = false;
+                searchQuery.parent().removeClass("open");
+                this.isLoading = false;
+                steal.dev.log("The search query size is smaller than 1. " +
+                    "Not showing the dropdwon.");
             }
         },
+        search: function () {
+            /*SearchboxModel.findAll(
+             {}, function( response ){
+             this.attr('hasResults', true);
+             this.attr('results', response[0].results);
+             this.attr('isLoading', false);
+             }.bind(this)
+             );*/
+        },
+        setLoading: function(loading){
+            this.isLoading = loading;
+        }
+    });
+    Component.extend({
+        tag: "nav-search-box",
+        view: template,
+        ViewModel: NavSearchViewModel.extend({
+            error: {value: false},
+            hasResults: {value: false},
+            retults: {value: []},
+            isLoading: {value: true},
+            previousValue: {value: ""},
+            searched: {value: false},
+        }),
         events: {
-
+            "inserted": function(searchComponent, event) {
+                steal.dev.log("Started");
+            },
             "#searchComponent focus": function(searchComponent, event) {
-                if($("#searchQuery").val().length > 2) {
-                    can_event.trigger($("#searchQuery"), "keyup");
-                }
                 steal.dev.log("Focused on the search Component!");
             },
-            "#searchComponent blur": function() {
-                steal.dev.log("Out of the search Component!");
+            "#searchDropdownMenu click": function() {
+                can_event.trigger.call($("#searchQuery")[0], "click");
+                steal.dev.log("Clicked on the dropdown menu!");
+                return false;
             },
-            "#searchComponent click.bs.dropdown": function(searchComponent,
-                                                           event) {
-                if($("#searchQuery").val().length > 2) {
-                    if($("#searchDropdown").hasClass("open")){
-                        $("#searchDropdown").toggleClass("open");
-                        steal.dev.log("The search query size is bigger than " +
-                            "2. Showing the dropdwon.");
-                    }
-                }
-                else {
-                    if(!$("#searchDropdown").hasClass("open")){
-                        $("#searchDropdown").toggleClass("open");
-                        steal.dev.log("The search query size is smaller " +
-                            "than 3. Not showing the dropdwon.");
-                    }
-                }
+            "#searchQuery click": function(searchQuery, event, buga) {
+                can_event.trigger.call(searchQuery, "keyup");
+                steal.dev.log("Clicked on the dropdown menu!");
+                return false;
             },
             "#searchQuery keyup": function(searchQuery, event) {
-                if(searchQuery.val().length > 2) {
-                    $("#searchDropdown").addClass("open");
-                    this.viewModel.isLoading = true;
-                    this.viewModel.search();
-                    steal.dev.log("The search query size is bigger than " +
-                        "2. Showing the dropdwon.");
-                }
-                else{
-                    $("#searchDropdown").removeClass("open");
-                    steal.dev.log("The search query size is smaller than 3. " +
-                        "Not showing the dropdwon.");
-                }
+                searchQuery = $(searchQuery);
+                this.viewModel.showSearchDropdown(searchQuery, event);
+                return false;
             }
         }
     });
